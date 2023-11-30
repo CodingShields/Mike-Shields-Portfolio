18c0dc7e6edc65478cb04db78a1471ebb8f93fbb
MIke Shields <codingshields@gmail.com>
2023-11-30 11:27:59 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+
+function App() {
+
+  return (
+      <div>
+
+      <p
+      className="text-2xl text-center text-blue-500"
+      >test</p>
+      </div>
+
+  );
+}
+
+export default App;
